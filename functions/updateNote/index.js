8ca737ee9809f754046a81eafda720a4156df853
ElDavido81 <eldavido81@gmail.com>
2025-11-26 18:36:03 +0100

    update-function fix

--- functions/updateNote/index.js
+++ functions/updateNote/index.js
@@ -9,19 +9,15 @@ const updateNote = async (event, context) => {
   if (event?.error && event?.error == '401')
   return sendResponse(401, {success: false, message: 'Invalid token!'});
 
-  const {userId, id, ...filteredAttributes} = JSON.parse(event.body);
+const {userId, id, ...filteredAttributes} = JSON.parse(event.body);
 
   filteredAttributes.modifiedAt = new Date().toISOString();
-
-  // const updateAttributes = JSON.parse(event.body);
 
   const updateExpression = 'set ' + Object.keys(filteredAttributes).map(attributeName => `${attributeName} = :${attributeName}`).join(', '); 
   const expressionAttributeValues = Object.keys(filteredAttributes).reduce((values , attributeName ) => {
       values[`:${attributeName}`] =  filteredAttributes[attributeName];
       return values;
   }, {});
-
-  // expressionAttributeValues[':modifiedAt'] = new Date().toISOString();
 
   try{
   await db.update({
